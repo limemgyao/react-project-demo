f46be23e48cb9cc7e925ac22d00341f65cf278b5
lixia <xia.li@huimeihealth.com.cn>
2021-03-31 17:00:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import './App.less';
+import 'antd/dist/antd.css';
+import {Button} from "antd";
+import './mock.js'
+import {get} from './common/http';
+import {message} from 'antd';
+import React from 'react';
+
+function App() {
+
+    /**
+     * 点击事件
+     */
+    const handleClick = () => {
+        // new Promise((resolve, reject) => {
+        //     get('/api/getData.json').then(res => {
+        //         if (res.code == '0') {
+        //             resolve(res.data);
+        //             console.log(res.data)
+        //         } else {
+        //             message.error(res.message);
+        //             resolve(res.data);
+        //         }
+        //     }).catch(res => {
+        //         message.error(res.message);
+        //     });
+        // });
+        get('./mock/BAK.json').then(res => {
+            if (res.code == '0') {
+                console.log(res.data)
+            } else {
+                message.error(res.message);
+            }
+        }).catch(res => {
+            message.error(res.message);
+        });
+    }
+
+    return (
+        <div className="App">
+            哈哈哈哈哈哈哈哈哈哈哈
+            <div className={'appCon'}>
+                <Button>嗯嗯嗯嗯额嗯嗯</Button>
+                <Button type="primary" onClick={handleClick}>嘿嘿嘿嘿嘿嘿嘿</Button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
